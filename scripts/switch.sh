52fc2e9589de8a7ad276236d0f7a5c31ecfa5281
NickFury001 <95432249+NickFury001@users.noreply.github.com>
2026-09-15 18:09:53 +0200

    fix: problematic switch script

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -3,10 +3,7 @@ set -euo pipefail
 cd "$(dirname "$0")/.."
 
 USER_NAME="$(whoami)"
-SYSTEM="$(nix eval --impure --expr 'builtins.currentSystem' --raw)"
 
 exec nix run home-manager/master -- switch \
   --flake "./home#${USER_NAME}" \
-  --override-input 'flake-arg:user'    "str:${USER_NAME}" \
-  --override-input 'flake-arg:system'  "str:${SYSTEM}" \
   --extra-experimental-features "nix-command flakes"
