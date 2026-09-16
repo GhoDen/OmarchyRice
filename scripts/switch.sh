076c1919060c727b3ace227cfeaa215b80f03028
Ghost of Denver <95432249+NickFury001@users.noreply.github.com>
2026-09-16 14:41:04 +0200

    enable autobackups

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -5,4 +5,4 @@ cd "$(dirname "$0")/.."
 USER_NAME="$(whoami)"
 
 exec nix run ./home#homeConfigurations."${USER_NAME}".activationPackage \
-  --extra-experimental-features "nix-command flakes"
+  --extra-experimental-features "nix-command flakes" -- -b backup
