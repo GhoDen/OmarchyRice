6367c29667a4763fb68bb59d213ebba34b3105c6
= <NickFury7500@gmail.com>
2026-09-19 23:42:49 +0200

    Remove unused biopass configs

--- dotfiles/nvim/lua/plugins/99.lua
+++ dotfiles/nvim/lua/plugins/99.lua
@@ -10,6 +10,7 @@ return 	{
 		local basename = vim.fs.basename(cwd)
 		_99.setup({
 			-- provider = _99.Providers.ClaudeCodeProvider,  -- default: OpenCodeProvider
+			model = "github-copilot/gpt-5.6-luna",
 			logger = {
 				level = _99.DEBUG,
 				path = "/tmp/" .. basename .. ".99.debug",
