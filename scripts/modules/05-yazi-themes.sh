e1f2882ee94b0959592934ac4f4569f13548247c
= <NickFury7500@gmail.com>
2026-09-22 17:53:38 +0200

    testing my own fixed fork of omarchy-yazi

--- scripts/modules/05-yazi-themes.sh
+++ scripts/modules/05-yazi-themes.sh
@@ -1,1 +1,1 @@
-curl -fsSL https://raw.githubusercontent.com/joaofelipegalvao/omarchy-yazi/main/scripts/omarchy-yazi-install.sh | bash
+curl -fsSL https://raw.githubusercontent.com/GhoDen/omarchy-yazi/main/scripts/omarchy-yazi-install.sh | bash
