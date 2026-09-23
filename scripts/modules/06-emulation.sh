59056c58700306a6736bc702a25f6c4342290584
= <NickFury7500@gmail.com>
2026-09-23 09:35:04 +0200

    reorder bash

--- scripts/modules/06-emulation.sh
+++ scripts/modules/06-emulation.sh
@@ -7,5 +7,5 @@ sudo systemctl enable --now libvirtd
 sudo usermod -aG libvirt "$USER"
 
 # Use 'sg' to run the virsh commands under the newly added 'libvirt' group
-sg libvirt -c "virsh net-autostart default"
 sg libvirt -c "virsh net-start default || true"
+sg libvirt -c "virsh net-autostart default"
